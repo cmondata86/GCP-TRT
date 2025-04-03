36930c475bf43e4bcd09db2c655c63be7406a1a4
unknown <trtjobs.mondal@gmail.com>
2025-04-03 15:15:30 -0400

    new again

--- gcs.tf
+++ gcs.tf
@@ -2,3 +2,10 @@
       project = "gcp-trt-training"
       region  = "us-east1"
     }
+resource "google_storage_bucket" "static" {
+ name          = "TRT-BUCKET1"
+ location      = "US"
+ storage_class = "STANDARD"
+
+ uniform_bucket_level_access = true
+}
