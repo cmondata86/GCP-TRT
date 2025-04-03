1c23863316461f6f15ad2ecc74c26c7c7d1b7e2b
unknown <trtjobs.mondal@gmail.com>
2025-04-03 16:09:17 -0400

    new again3

--- gcs.tf
+++ gcs.tf
@@ -3,10 +3,10 @@
       region  = "us-east1"
     }
 resource "google_storage_bucket" "bucket1" {
- name          = "TRT-BUCKET1"
+ name          = "trt-bucket1"
  location      = "us-east1"
 }
 resource "google_storage_bucket" "bucket2" {
- name          = "TRT-BUCKET2"
+ name          = "trt-bucket2"
  location      = "us-east1"
 }
