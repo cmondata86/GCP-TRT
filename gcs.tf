e71e7a3f3dbab488404f1326d46118d596ccc893
unknown <trtjobs.mondal@gmail.com>
2025-04-03 14:59:40 -0400

    test terraform cloud

--- gcs.tf
+++ gcs.tf
@@ -2,3 +2,11 @@
       project = "gcp-trt-training"
       region  = "us-east1"
     }
+
+    resource "google_storage_bucket" "static" {
+ name          = "BUCKET_NAME"
+ location      = "US"
+ storage_class = "STANDARD"
+
+ uniform_bucket_level_access = true
+}
