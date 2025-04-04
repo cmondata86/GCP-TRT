06838d7408fbbee5cb8ea2cb88e9d080954ffc03
unknown <trtjobs.mondal@gmail.com>
2025-04-04 12:28:30 -0400

    new again8

--- gcs.tf
+++ gcs.tf
@@ -2,17 +2,6 @@
       project = "gcp-trt-training"
       region  = "us-east1"
     }
-
-#    terraform {
-#  backend "remote" {
-#    hostname = "app.terraform.io"
-#    organization = "Demo_GCP-TRT"
-#
-#    workspaces {
-#      name = "GCP-TRT-Tera-Cloud"
-#    }
-#  }
-#}
 resource "google_storage_bucket" "bucket1" {
  name          = "trt-bucket1"
  location      = "us-east1"
