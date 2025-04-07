b6166b2291a761c38df565eedff6f9871e12df35
unknown <trtjobs.mondal@gmail.com>
2025-04-07 16:21:02 -0400

    new again16

--- gcs.tf
+++ gcs.tf
@@ -17,3 +17,26 @@ resource "google_project_service" "alloydb_api" {
   service = "alloydb.googleapis.com"
   depends_on = [google_project_service.cloudresourcemanager_api]
 }
+resource "google_alloydb_cluster" "trt_alloy_cluster" {
+  cluster_id   = "trt_alloy_cluster"
+  #region       = "us-east1"
+  location     = "us-east1-a"
+  #network      = "default"
+}
+
+resource "google_alloydb_user" "trt_alloy_user" {
+  cluster  = google_alloydb_cluster.trt_alloy_cluster.id
+  user_id  = "postgres"
+  password = "postgres123" # Replace with a secure password
+  user_type = "ALLOYDB_BUILT_IN" # Specify the user type
+}
+
+resource "google_alloydb_instance" "trt_alloy_instance" {
+  instance_id  = "trt_alloy_instance"
+  cluster      = google_alloydb_cluster.trt_alloy_cluster.id
+ # region       = google_alloydb_cluster.trt_alloy_cluster.region
+  instance_type = "PRIMARY"
+  machine_config {
+    cpu_count = 2
+  }
+}
