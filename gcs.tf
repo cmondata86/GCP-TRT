0bc63e4bad0a68cfd698d7574d81754219119ccf
unknown <trtjobs.mondal@gmail.com>
2025-04-07 18:46:28 -0400

    new again21

--- gcs.tf
+++ gcs.tf
@@ -30,6 +30,32 @@ resource "google_project_service" "service_networking_api" {
   depends_on = [google_project_service.cloudresourcemanager_api]
 }
 
+resource "google_project_iam_member" "servicenetworking_admin" {
+  project = "gcp-trt-training" # Replace with your project ID
+  role    = "roles/servicenetworking.networksAdmin"
+  members = [
+    "user:trtjobs.mondal@gmail.com",          # Replace with the first user
+    "serviceAccount:292380354815-compute@developer.gserviceaccount.com",          # Replace with the second user
+    "serviceAccount:gcp-trt-training@gcp-trt-training.iam.gserviceaccount.com"   # Replace with a service account
+  ]
+}
+
+resource "google_project_iam_binding" "compute_network_admin" {
+  project = "gcp-trt-training" # Replace with your project ID
+  role    = "roles/compute.networkAdmin"
+
+  members = [
+    "user:trtjobs.mondal@gmail.com",          # Replace with the first user
+    "serviceAccount:292380354815-compute@developer.gserviceaccount.com",          # Replace with the second user
+    "serviceAccount:gcp-trt-training@gcp-trt-training.iam.gserviceaccount.com"   # Replace with a service account
+  ]
+}
+
+resource "google_project_iam_member" "servicenetworking_admin" {
+  project = "gcp-trt-training" # Replace with your project ID
+  role    = "roles/servicenetworking.networksAdmin"
+  member  = "user:trtjobs.mondal@gmail.com" # Replace with the user, service account, or group
+}
 
 resource "google_compute_network" "private_network" {
   name = "trt-private-network"
