3776209d2630e5dc4f727f0bc630b6dc84394dfd
unknown <trtjobs.mondal@gmail.com>
2025-04-07 15:00:37 -0400

    new again13

--- gcs.tf
+++ gcs.tf
@@ -6,3 +6,24 @@ resource "google_storage_bucket" "bucket1" {
  name          = "trt-bucket1"
  location      = "us-east1"
 }
+
+resource "google_alloydb_cluster" "trt_alloy_cluster" {
+  cluster_id   = "trt_alloy_cluster"
+  region       = "us-east1"
+  network      = "default"
+
+  initial_user {
+    username = "postgres"
+    password = "postgres123" # Replace with a secure password
+  }
+}
+
+resource "google_alloydb_instance" "trt_alloy_instance" {
+  instance_id  = "trt_alloy_instance"
+  cluster      = google_alloydb_cluster.trt_alloy_cluster.id
+  region       = google_alloydb_cluster.trt_alloy_cluster.region
+  instance_type = "PRIMARY"
+  machine_config {
+    cpu_count = 2
+  }
+}
