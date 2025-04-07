c634339104a77f2fcd19e84f801f801b8e01f673
unknown <trtjobs.mondal@gmail.com>
2025-04-07 15:25:17 -0400

    new again14

--- gcs.tf
+++ gcs.tf
@@ -12,11 +12,12 @@ resource "google_alloydb_cluster" "trt_alloy_cluster" {
   #region       = "us-east1"
   location     = "us-east1-a"
   #network      = "default"
+}
 
-  initial_user {
-    username = "postgres"
-    password = "postgres123" # Replace with a secure password
-  }
+resource "google_alloydb_user" "trt_alloy_user" {
+  cluster  = google_alloydb_cluster.trt_alloy_cluster.id
+  user_id  = "postgres"
+  password = "postgres123" # Replace with a secure password
 }
 
 resource "google_alloydb_instance" "trt_alloy_instance" {
