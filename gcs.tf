973d8fc3b5284918021745d5460842df38d9442b
unknown <trtjobs.mondal@gmail.com>
2025-04-07 17:27:20 -0400

    new again19

--- gcs.tf
+++ gcs.tf
@@ -66,6 +66,20 @@ resource "google_project_service" "service_networking_api" {
   depends_on = [google_project_service.cloudresourcemanager_api]
 }
 
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.private_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_range.name]
+}
+
+resource "google_compute_global_address" "private_ip_range" {
+  name          = "private-ip-range"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.private_network.id
+}
+
 resource "google_alloydb_instance" "alloy_instance" {
   instance_id   = "trt-alloy-instance"
   cluster       = google_alloydb_cluster.alloy_cluster.id
