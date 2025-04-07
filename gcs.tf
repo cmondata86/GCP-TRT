40ae633967a3a1d84afe754271f349b8f8507ed4
unknown <trtjobs.mondal@gmail.com>
2025-04-07 15:35:29 -0400

    new again15

--- gcs.tf
+++ gcs.tf
@@ -7,6 +7,11 @@ resource "google_storage_bucket" "bucket1" {
  location      = "us-east1"
 }
 
+resource "google_project_service" "cloudresourcemanager_api" {
+  project = "gcp-trt-training" # Replace with your project ID
+  service = "cloudresourcemanager.googleapis.com"
+}
+
 resource "google_project_service" "alloydb_api" {
   project = "gcp-trt-training" # Replace with your project ID
   service = "alloydb.googleapis.com"
