a488de531725037899628fa9a9ad60349b512ced
unknown <trtjobs.mondal@gmail.com>
2025-04-08 14:43:38 -0400

    new again23

--- gcs.tf
+++ gcs.tf
@@ -173,3 +173,16 @@ resource "google_compute_instance" "psql_instance" {
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 }
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.private_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.235.240.0/20"] # IAP IP range
+  target_tags   = ["psql-instance"]   # Ensure the VM has this tag
+}
